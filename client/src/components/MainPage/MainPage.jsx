c2a1f50efbfe5262fe42525942f5594b5ea57b86
Polina <bad.black.neko.xx@gmail.com>
2023-01-30 18:07:42 +0600

    First block done

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from "./MainPage.module.css";
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 
@@ -6,6 +7,11 @@ export default function MainPage() {
   return (
     <>
         <Header/>
+        <div className={styles.first_block}>
+            <div><img src="/images/big_logo.png" alt='logo'/></div>
+            <div>Experience the perfect combination of modern <br/>housing & natural scenery.</div>
+            <div>Located in Lofoten, Norway</div>
+        </div>
         <Footer/>
     </>
   )
